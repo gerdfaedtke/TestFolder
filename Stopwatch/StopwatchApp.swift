8834e9096fc9155d01c7c9314060494739f735ff
Gerd Faedtke <gerd.faedtke@icloud.com>
2023-02-28 14:29:08 +0100

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Gerd Faedtke on 28.02.23.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
